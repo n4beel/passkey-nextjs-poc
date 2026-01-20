8f35883c0460b3850274e091fb1bac439eb4356d
Nabeel <nabeel3353045805@hotmail.com>
2026-01-20 14:27:15 +0500

    feat: Add a rewrite rule to proxy `.well-known` paths to the configured API URL.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/.well-known/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
